a5441332d75fc255668468915c172c956ae570c3
Ninh Le <levanninh0101@gmail.com>
2017-10-12 21:05:01 +0700

    Change Offset for pagination from 1 to 0

--- LandonApi/Models/PagingOptions.cs
+++ LandonApi/Models/PagingOptions.cs
@@ -8,7 +8,7 @@ namespace LandonApi.Models
 {
     public class PagingOptions
     {
-        [Range(1, 9999, ErrorMessage = "Offset must be greater than 0.")]
+        [Range(0, 9999, ErrorMessage = "Offset must be greater or equal 0.")]
         public int? Offset { get; set; }
 
         [Range(1, 100, ErrorMessage = "Limit must be greater than 0 and less than 100.")]
